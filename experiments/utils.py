7f798ec55d5a6bb5c49baf6a1d3caa16e14f2213
misakiyanan <49162102+misakiyanan@users.noreply.github.com>
2021-04-04 22:43:09 +1000

    Add files via upload

---
+++ experiments/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+## create supervised dataframe
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+  # drop nan
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
